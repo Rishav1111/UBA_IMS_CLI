256655333c33638c4af43313e7a0b152f2166997
RishavSht <np03cs4s220093@heraldcollege.edu.np>
2024-06-14 13:00:08 +0545

    CLI CRUD without using extra modules

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const { Command } = require("commander");
 const fs = require("fs");
-const program = new Command();
 
 //defing the path of the JSON file where the users data is stored
 const USERS_FILE = "users.json";
@@ -20,91 +18,138 @@ const writeUsers = (users) => {
   fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
 };
 
-// Command to create a new user
-program
-  .command("user:create")
-  .description("Create a new user")
-  .requiredOption("-f, --fname <First Name>", "First name")
-  .requiredOption("-l, --lname <Last Name>", "Last name")
-  .action((options) => {
-    const { fname, lname } = options; //Destructuring options to extact fname and lname
-    if (!fname || !lname) {
-      console.log("Both fname and lname are required");
-      return;
-    }
+// Function to display usage information
+function showUsage() {
+  console.log(`
+Usage:
+  node index.js user:create -f <First Name> -l <Last Name>
+  node index.js user:lists
+  node index.js user:update -f <Current First Name> -nf <New First Name> -l <New Last Name>
+  node index.js user:delete [-f <First Name>] [--all]
 
-    const users = readUsers();
-    users.push({ fname, lname });
-    writeUsers(users);
-    console.log(`User ${fname} ${lname} created successfully.`);
-  });
+Commands:
+  user:create   Create a new user
+  user:lists    List all users
+  user:update   Update a user by their first name
+  user:delete   Delete a user by their first name or delete all users
+  `);
+}
 
-// Command to list all the users from the file
-program
-  .command("user:lists")
-  .description("list all the users.")
-  .action(() => {
-    const users = readUsers();
-    console.log("All the lists of users:", users);
-  });
+// Parsing command-line arguments manually
+const args = process.argv.slice(2); // Extract arguments, skipping 'node' and script name
+const command = args[0];
 
-//command to update the user by thier firstname
-program
-  .command("user:update")
-  .description("Update the user by their first name")
-  .requiredOption(
-    "-f, --fname <fname>",
-    "Current first name of the user to update"
-  )
-  .requiredOption("-n, --newFname <newFname>", "New first name")
-  .requiredOption("-l, --newLname <newLname>", "New last name")
-  .action((options) => {
-    //when the command is executed, this action is
+// Handling different commands based on the first argument
+switch (command) {
+  //command to create a new users
+  case "user:create":
+    {
+      // Finding  the index of the first name and last name options
+      const fnameIndex = args.indexOf("-f");
+      const lnameIndex = args.indexOf("-l");
 
-    const { fname, newFname, newLname } = options; //Destructuring options to extact 'fname', 'newFname', and 'newLname'.
-    const users = readUsers();
+      if (fnameIndex !== -1 && lnameIndex !== -1) {
+        //extracting the values for firstname and lastname
+        const fname = args[fnameIndex + 1];
+        const lname = args[lnameIndex + 1];
 
-    // Find the index of the user with the current first name.
-    const userIndex = users.findIndex((user) => user.fname === fname);
-    if (userIndex !== -1) {
-      //Check if the user exists (index found).
-      users[userIndex] = { fname: newFname, lname: newLname };
-      writeUsers(users);
-      console.log(`User ${fname} updated to ${newFname} ${newLname}`);
-    } else {
-      console.log(`User ${fname} not found.`);
-    }
-  });
+        if (!fname || !lname) {
+          console.log("Both first name and last name are required.");
+          break;
+        }
 
-//command to delete specific user or all the users
-program
-  .command("user:delete")
-  .description("Delete a user")
-  .option("-f, --fname <First name>", "First name of the user to delete")
-  .option("--all", "Delete all users")
-  .action((options) => {
-    let users = readUsers();
-
-    if (options.all) {
-      // Check if the --all flag is set.
-      users = []; // Clear all users
-      console.log("All users deleted.");
-    } else {
-      const originalLength = users.length;
-
-      // Filtering out the user with the specified first name.
-      users = users.filter((user) => user.fname !== options.fname);
-      const newLength = users.length;
-
-      if (originalLength === newLength) {
-        console.log(`User with first name ${options.fname} not found.`);
+        const users = readUsers();
+        users.push({ fname, lname });
+        writeUsers(users);
+        console.log(`User ${fname} ${lname} created successfully.`);
       } else {
-        console.log(`User ${options.fname} deleted.`);
+        console.log("Missing required options for creating a user.");
       }
     }
+    break;
 
-    writeUsers(users);
-  });
+  //command to list all the users
+  case "user:lists":
+    {
+      const users = readUsers();
+      console.log("All the lists of users:", users);
+    }
+    break;
 
-// Parsing the command-line arguments
-program.parse(process.argv);
+  //command to update a user by their firtnme
+  case "user:update":
+    {
+      //Finding the indexs
+      const fnameIndex = args.indexOf("-f");
+      const newFnameIndex = args.indexOf("-nf");
+      const newLnameIndex = args.indexOf("-l");
+
+      if (fnameIndex !== -1 && newFnameIndex !== -1 && newLnameIndex !== -1) {
+        //extracting the values
+        const fname = args[fnameIndex + 1];
+        const newFname = args[newFnameIndex + 1];
+        const newLname = args[newLnameIndex + 1];
+
+        if (!fname || !newFname || !newLname) {
+          console.log("All name fields are required.");
+          break;
+        }
+
+        const users = readUsers();
+        //finding the index of the user
+        const userIndex = users.findIndex((user) => user.fname === fname);
+
+        //Check if the user exists.
+        if (userIndex !== -1) {
+          users[userIndex] = { fname: newFname, lname: newLname };
+          writeUsers(users);
+          console.log(`User ${fname} updated to ${newFname} ${newLname}.`);
+        } else {
+          console.log(`User ${fname} not found.`);
+        }
+      } else {
+        console.log("Missing required options for updating a user.");
+      }
+    }
+    break;
+
+  //command to delete a user or all users
+  case "user:delete":
+    {
+      const fnameIndex = args.indexOf("-f");
+      const allIndex = args.indexOf("--all");
+
+      let users = readUsers();
+
+      if (allIndex !== -1) {
+        //if -all then delete all users
+        users = []; // Clear all users
+        console.log("All users deleted.");
+      } else if (fnameIndex !== -1) {
+        //if -f then delete specific user
+        const fname = args[fnameIndex + 1];
+        const originalLength = users.length;
+
+        // Filtering out the user with the specified first name.
+        users = users.filter((user) => user.fname !== fname);
+        const newLength = users.length;
+
+        if (originalLength === newLength) {
+          console.log(`User with first name ${fname} not found.`);
+        } else {
+          console.log(`User ${fname} deleted.`);
+        }
+      } else {
+        console.log(
+          "Specify a user to delete with -f or use --all to delete all users."
+        );
+      }
+
+      writeUsers(users);
+    }
+    break;
+
+  default:
+    showUsage();
+    break;
+}
